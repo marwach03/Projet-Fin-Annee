88a9246622e440aaaee8c96e225a6e19644435d3
ghitatag03 <ghitatag00@gmail.com>
2024-05-23 00:11:27 +0100

    ToDoModalco ok

---
+++ frontend/screens/todolist/AddListModal.js
@@ -0,0 +1,112 @@
+import { Text, View, StyleSheet, KeyboardAvoidingView, TouchableOpacity, TextInput } from 'react-native';
+import React, { Component } from 'react';
+import { AntDesign } from '@expo/vector-icons';
+import TempData from './TempData';  // Assurez-vous que le chemin est correct
+
+export class AddListModal extends React.Component {
+    backgroundColors = ["#5CD859", "#008080", "#24A6D9", "#8022D9", "#D159D8", "#D85963", "#D88559"];
+    
+    state = {
+        name: "",
+        color: this.backgroundColors[0]
+    }
+
+    createTodo = () => {
+        const { name, color } = this.state;
+        
+        
+        if (name.length > 0) {
+            const list = {name,color}
+            
+            this.props.addList(list);
+
+            console.log("Updated TempData:", TempData);
+
+            this.setState({ name: "" });
+            this.props.closeModal();
+        } else {
+            alert("List name cannot be empty!");
+        }
+    };
+
+    renderColors() {
+        return this.backgroundColors.map(color => {
+            return (
+                <TouchableOpacity
+                    key={color}
+                    style={[styles.colorSelect, { backgroundColor: color }]}
+                    onPress={() => this.setState({ color })}
+                />
+            )
+        });
+    }
+
+    render() {
+        return (
+            <KeyboardAvoidingView style={styles.container} behavior="padding">
+                <TouchableOpacity style={{ position: "absolute", top: 64, right: 32 }} onPress={this.props.closeModal}>
+                    <AntDesign name='close' size={24} color={'#008080'} />
+                </TouchableOpacity>
+
+                <View style={{ alignSelf: "stretch", marginHorizontal: 32 }}>
+                    <Text style={styles.title}>Create Todo List</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="List Name?"
+                        onChangeText={text => this.setState({ name: text })}
+                    />
+
+                    <View style={{ flexDirection: "row", justifyContent: "space-between", marginTop: 12 }}>
+                        {this.renderColors()}
+                    </View>
+
+                    <TouchableOpacity style={[styles.create, { backgroundColor: this.state.color }]} onPress={this.createTodo}>
+                        <Text style={styles.createText}>Create!</Text>
+                    </TouchableOpacity>
+                </View>
+            </KeyboardAvoidingView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      alignItems:"center",
+      justifyContent:"center",
+    },
+    title:{
+        fontSize: 28,
+        fontWeight: "800",
+        color: 'black',
+        alignSelf: "center",
+        marginBottom: 16
+    },
+    input: {
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor:'#008080',
+        borderRadius: 6,
+        height: 50,
+        marginTop:8,
+        paddingHorizontal:16,
+        fontSize: 18
+    },
+    aa:{
+        color: 'White',
+        fontWeight: "600"
+    },
+    create:{
+        marginTop: 24,
+        height: 50,
+        borderRadius: 6,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    colorSelect:{
+        width: 30,
+        height: 30,
+        borderRadius: 4
+    }
+});
+
+export default AddListModal
